2179356a3fcc0113fcca2041a0381961ce6289a8
FiorelaPintoM25 <166184310+FiorelaPintoM25@users.noreply.github.com>
2024-04-08 17:49:11 +0000

    commit I

---
+++ solution/server.py
@@ -0,0 +1,28 @@
+from http.server import HTTPHandler,BaseHTTPRequestHandler
+#for x in range(10) for y in range(x, x+10)).
+
+
+
+
+
+
+
+
+
+Jugador(self,id,player,number,attempts,status)
+    self.id=id
+    self.player=player
+    self.number=number
+    self.attempts=attempts
+    self.status=status
+
+Adivinar(self,numero)
+    numeroS=10
+    if numero==numeroS
+        return "Felicitaciones"
+    else if(numero<numeroS)
+             return "El numero a adivinar es Mayor"
+             else 
+                return "El numero a adivinar es menor"
+    
+
